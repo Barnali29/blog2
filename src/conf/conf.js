35f48a8bfbbec47340f509391971c8bad33274bd
Barnali29 <barnali03b@gmail.com>
2024-07-09 18:54:37 +0530

    done

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf={
+    appwriteurl:String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteproject_id:String(process.env.REACT_APP_PROJECT_ID),
+    appwritecollection_id:String(process.env.REACT_APP_COLLECTION_ID),
+    appwritedatabase_id:String(process.env.REACT_APP_DATABASE_ID),
+    appwritebucket_id:String(process.env.REACT_APP_BUCKET_ID),
+}
+
+export default conf
